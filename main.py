305ee8e009477cb3aec4ba849dc484cc6f854384
Katie Sipos <ohitsmekatie@github.com>
2020-10-25 15:45:55 +0000

    formatting and spacing

--- main.py
+++ main.py
@@ -6,30 +6,31 @@ Homework - midterm
 A todo list program that let's a user add, remove and view their list 
 '''
 
-# TODOS: 
-# if they enter a string use isnumeric = false before you assign cast to int 
-# print user friendly numbers instead of python friendly starting at 0 
+# TODOS:
+# if they enter a string use isnumeric = false before you assign cast to int
+# print user friendly numbers instead of python friendly starting at 0
+
 
 def add_to_list(todo_list):
     ''' a function that gets a task from a user and adds it to their todo list'''
-    # get task from user and store in variable 
+    # get task from user and store in variable
     task = input('What is your task? ')
     # checks if task is empty and prompts user again if they just hit enter
     if len(task) == 0:
-      task = input('You did not type anything! Enter text for your task: ')
-    # append task to the todo list 
+        task = input('You did not type anything! Enter text for your task: ')
+    # append task to the todo list
     todo_list.append(task)
 
 
 def view_list(printable_list):
     ''' this is a function that takes a generic list and formats it to print'''
-    # start number at -1 so it works with proper indices 
+    # start number at -1 so it works with proper indices
     number = -1
-    # if the list has nothing in it, but they choose the option let them know they don't have anything on the list 
+    # if the list has nothing in it, but they choose the option let them know they don't have anything on the list
     if len(printable_list) == 0:
-      print('\nYou do not have anything on your list!')
+        print('\nYou do not have anything on your list!')
     print('\n\nYour list items are:\n')
-    # for loop that prints a pretty list 
+    # for loop that prints a pretty list
     for item in printable_list:
         number += 1
         print(number, ': ', item, sep=' ')
@@ -37,7 +38,7 @@ def view_list(printable_list):
 
 def remove_from_list(todo_list, completed_list):
     ''' a funtion that asks a user what they want to remove, removes that from the current todo list, and adds it to a completed list'''
-    # print list for user 
+    # print list for user
     view_list(todo_list)
     # ask the user what they want to remove
     user_choice = int(input('\nWhat number would you like to remove? '))
@@ -55,7 +56,7 @@ def quit_program():
 
 def main():
     print('Welcome the Todo List Application!\n')
-    # create all variables for program for current list, completed list, and the user's choice 
+    # create all variables for program for current list, completed list, and the user's choice
     todo_list = []
     completed_list = []
     user_choice = 0
@@ -69,7 +70,7 @@ def main():
         4. View your completed list\n   
         5. Quit program (NOTE: your list will not be saved)\n 
         ''')
-        # get user choice and decide what part of the program to run 
+        # get user choice and decide what part of the program to run
         user_choice = int(input('What would you like to do? '))
         if user_choice == 1:
             add_to_list(todo_list)
